799ff44b638acded81b887ab19c3e8e58c668a73
Luke Prior <lukeprior3@gmail.com>
2023-03-04 16:32:45 +1100

    feat: wip

--- client/src/pages/Breakfast.tsx
+++ client/src/pages/Breakfast.tsx
@@ -7,17 +7,56 @@ import row4 from '../assets/row4.png';
 import info from '../assets/whiteinfo.png';
 import logo from '../assets/thicklogo.png';
 import FormPage from '../components/FormPage';
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
+
+const style = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: '45vw',
+  bgcolor: '#D9D9D9',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
 const Breakfast = () => {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   return (
     <div className='page'>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+        <img src={info} className='info' />
+          <h1>
+            This is a demo of a web app that helps you track your food intake
+          </h1>
+          <img src={info} className='info' />
+          <p>
+            This is a demo of a web app that helps you track your food intake
+            and make sure you are eating a balanced diet. This is a demo of a web
+            app that helps you track your food intake and make sure you are
+            eating a balanced diet. This is a demo of a web app that helps you
+            </p>
+            <button className='purple-button' onClick={handleClose}>close</button>
+        </Box>
+      </Modal>
       <div>
         <img src={row1} className='row' />
         <img src={row2} className='row' />
       </div>
       <div className='container'>
         <div className='top-bar'>
-          <img src={info} className='info' />
+          <img src={info} onClick={handleOpen} className='info' />
         </div>
         <div className='main-text'>
           <FormPage
