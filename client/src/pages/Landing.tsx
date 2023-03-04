a878c0d6c79c3c2769ba5e7323015e1401858a06
wanning <z5371162@unsw.edu.au>
2023-03-04 11:59:04 +1100

    working on title

--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -1,12 +1,35 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import row1 from '../assets/row1.png';
+import row2 from '../assets/row2.png';
+import row3 from '../assets/row3.png';
+import row4 from '../assets/row4.png';
 
 const Landing = () => {
   return (
-    <div>
-      <Link to='/breakfast'>
-        <button>Let's get started</button>
-      </Link>
+    <div className='page'>
+      <img src={row1} />
+      <img src={row2} />
+      <div className='container'>
+        <div className='top-bar'>
+          <button>info</button>
+        </div>
+        <hr />
+        <div className='main-text'>
+          <h1>TITLE FILLER</h1>
+          <h3>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.{' '}
+          </h3>
+          <Link to='/breakfast'>
+            <button className='purple-button'>Let's get started</button>
+          </Link>
+        </div>
+      </div>
+      <img src={row3} />
+      <img src={row4} />
     </div>
   );
 };
