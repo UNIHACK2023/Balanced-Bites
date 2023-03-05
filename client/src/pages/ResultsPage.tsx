1c1f3586c0f28dac2270911bfef5333555cdbb2e
wanning <z5371162@unsw.edu.au>
2023-03-05 12:12:19 +1100

    progress for now WIP

--- client/src/pages/ResultsPage.tsx
+++ client/src/pages/ResultsPage.tsx
@@ -14,7 +14,7 @@ const NewResults = () => {
   return (
     <div className='page'>
       <InfoModal open={showModal} handleClose={handleClose}>
-        <h1>Details</h1>
+        <h1>About</h1>
         <p>TODO</p>
       </InfoModal>
       <div style={{ display: 'flex' }}>
